f8e3dc1c595c48cd4b301d13aeb2d3f90443e8de
Alberto Navarro <alnavarro@BookAir.local>
2013-11-12 02:01:44 -0500

    Initial Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# sailsRelations
+### a Sails application
---
+++ api/models/Employee.js
@@ -0,0 +1,22 @@
+/**
+ * Employee
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+   attributes: {
+  	
+  	name: { type: 'string', required: true },
+
+  	info: 'string', description: 'string',
+
+  	company: { model: 'company' }   	
+    
+  }
+
+
+};
